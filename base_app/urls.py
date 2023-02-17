606aa173ee332721e9900e19b09c9f4bb0fb5924
cruzetapiko <120158874+cruzetapiko@users.noreply.github.com>
2023-02-17 02:24:39 +0000

    registration-login-restrictions completed

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import TaskList
+from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', TaskList.as_view(),name='index'),
+    path('login/',UserLogin.as_view(), name='login'),
+    path('logout/',LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegistrationPage.as_view() ,name='register'),
+
+    path('', TaskList.as_view(), name='index'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.shortcuts import render, redirect
 from .models import Task
 
-# Create your views here.
-class TaskList(ListView):
+from django.views.generic.list import ListView 
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+#User Registration methods
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+#user login
+class  UserLogin(LoginView):
+    template_name = 'base_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+#user registration
+class RegistrationPage(FormView):
+    template_name = 'base_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user=True
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegistrationPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegistrationPage, self).get(self, *args, **kwargs)
+        
+
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     template_name = 'base_app/index.html'
     context_object_name = 'list'
